7a3637b73f84f1bd5eefb9e2b1ec19ff4f84547c
Boizi-06 <hson05542@gmail.com>
2024-11-21 12:59:41 +0700

    Add files via upload

---
+++ btvn01ss2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+	// kieu du lieu so nguyen 
+	int Hoang_Son_Dang_Co_Bao_Nhieu_Tien;
+	//kieu du lieu so thuc
+	float Hoang_Son_Qua_Dep_Trai;
+	double AE_Cai_Bang_Vo_Dich;
+	///kieu du lieu ki tu
+	char Thay_Cuong_Dtrai_Khong_Bang_Hoang_Son;
+	
+	return 0;
+}
